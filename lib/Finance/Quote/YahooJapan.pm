aed053101d65f7df877f452971130ba316b4cc2a
LiosK <contact@mail.liosk.net>
2009-06-21 00:33:16 +0900

    Under Development...

--- lib/Finance/Quote/YahooJapan.pm
+++ lib/Finance/Quote/YahooJapan.pm
@@ -1,5 +1,8 @@
 #!/usr/bin/perl -w
 
+# Author:   LiosK <contact@mail.liosk.net>
+# License:  The GNU General Public License
+
 package Finance::Quote::YahooJapan;
 
 use strict;
@@ -17,7 +20,7 @@ sub methods {
 }
 
 sub labels {
-    return (yahoo_japan => []);
+    return (yahoo_japan => ['method', 'success', 'date', 'currency', 'price']);
 }
 
 sub yahoo_japan {
@@ -28,12 +31,19 @@ sub yahoo_japan {
     my %info = ();
     my $ua = $quoter->user_agent;
 
+    # my $sym = shift @symbols;
+    # $info{$sym, 'price'} = 5000;
+    # $info{$sym, 'currency'} = 'JPY';
+    # $info{$sym, 'date'} = '2009-06-20';
+    # $info{$sym, 'success'} = 1;
+    # $info{$sym, 'method'} = 'yahoo_japan';
+
     # A request can contain less than 51 symbols.
     while (my @syms = splice @symbols, 0, 50) {
         my $url = $YAHOO_JAPAN_URL . '?s=' . join '+', @syms;
         my $reply = $ua->request(GET $url);
         if ($reply->is_success) {
-            _scrape(\%info, decode 'euc-jp', $reply->content);
+            _scrape(\%info, $reply->content, @syms);
         }
     }
 
@@ -42,8 +52,11 @@ sub yahoo_japan {
 }
 
 sub _scrape {
-    my $info = shift;
-    $info->{'hello'} = encode 'sjis', shift;
+    my ($info, $content, @symbols) = @_;
+
+    # Extracting price list table.
+    ($content) = $content =~ /<tr class=chartbg>(.+?)<\/table>/s;
+    my @list = grep /^<td/, split /\x0D?\x0A/, $content;
 }
 
 1;
