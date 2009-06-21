813b58960d8c6207e5452b0f0a0aef2f9495fe88
LiosK <contact@mail.liosk.net>
2009-06-22 00:19:48 +0900

    Minimum functionality to get mutual fund prices was implemented.

--- lib/Finance/Quote/YahooJapan.pm
+++ lib/Finance/Quote/YahooJapan.pm
@@ -20,30 +20,24 @@ sub methods {
 }
 
 sub labels {
-    return (yahoo_japan => ['method', 'success', 'date', 'currency', 'price']);
+    return (yahoo_japan => ['method', 'success', 'name', 'date', 'currency', 'price']);
 }
 
 sub yahoo_japan {
-    my $quoter = shift;
-    my @symbols = @_;
+    my ($quoter, @symbols) = @_;
     return unless @symbols; # Nothing if no symbols.
 
     my %info = ();
     my $ua = $quoter->user_agent;
-
-    # my $sym = shift @symbols;
-    # $info{$sym, 'price'} = 5000;
-    # $info{$sym, 'currency'} = 'JPY';
-    # $info{$sym, 'date'} = '2009-06-20';
-    # $info{$sym, 'success'} = 1;
-    # $info{$sym, 'method'} = 'yahoo_japan';
 
     # A request can contain less than 51 symbols.
     while (my @syms = splice @symbols, 0, 50) {
         my $url = $YAHOO_JAPAN_URL . '?s=' . join '+', @syms;
         my $reply = $ua->request(GET $url);
         if ($reply->is_success) {
-            _scrape(\%info, $reply->content, @syms);
+            # The way to extract quotes from a HTTP response is defined in
+            # another subroutine because it is quite likely to be modified.
+            %info = (%info, _scrape($reply->content, @syms));
         }
     }
 
@@ -51,12 +45,36 @@ sub yahoo_japan {
     return \%info;
 }
 
-sub _scrape {
-    my ($info, $content, @symbols) = @_;
+# Scrapes quotes from a HTML text.
+sub _scrape($;@) {
+    my ($content, @symbols) = @_;
+    my %info = ();
 
-    # Extracting price list table.
+    # Extracts price list table.
+    # XXX: Using an ugly, inflexible and unsophisticated algorithm.
     ($content) = $content =~ /<tr class=chartbg>(.+?)<\/table>/s;
-    my @list = grep /^<td/, split /\x0D?\x0A/, $content;
+    my @table = grep /^<td/, split /\x0D?\x0A/, $content;
+
+    foreach my $row (@table) {
+        $row =~ s/&nbsp;|<[^>]+?>/ /g;  # Stripping tags and NBSPs.
+        my (undef, $sym, undef, $name, $date, $price) = split /\s+/, $row;
+
+        # Formats data.
+        $date .= '/2009';   # TODO
+        $price =~ s/,//g;   # TODO
+
+        # Validates data.
+        # TODO
+
+        $info{$sym, 'success'}  = 1;
+        $info{$sym, 'currency'} = 'JPY';
+        $info{$sym, 'method'}   = 'yahoo_japan';
+        $info{$sym, 'name'}     = $name;
+        $info{$sym, 'date'}     = $date;
+        $info{$sym, 'price'}    = $price;
+    }
+
+    return %info;
 }
 
 1;
