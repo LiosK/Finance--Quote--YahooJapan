396b569bd1dff0ac6d1b295e6dd25625a758af0c
LiosK <contact@mail.liosk.net>
2012-10-27 23:38:33 +0900

    Changed pagination-related algorithm to decrease redundant HTTP requests.
    Signed-off-by: LiosK <contact@mail.liosk.net>

--- lib/Finance/Quote/YahooJapan.pm
+++ lib/Finance/Quote/YahooJapan.pm
@@ -4,57 +4,90 @@
 # License:  The GNU General Public License
 #
 # Information obtained by this module may be covered by Yahoo's terms
-# and conditions. See http://quote.yahoo.co.jp/ for more details.
+# and conditions. See http://finance.yahoo.co.jp/ for more details.
 
 package Finance::Quote::YahooJapan;
 
 use strict;
 use warnings;
 use utf8;
+use HTML::TreeBuilder;
 use HTTP::Request::Common;
 
 our $VERSION = '0.4';
 
-our $YAHOO_JAPAN_URL = 'http://info.finance.yahoo.co.jp/search';
+# The maximum number of symbols a search query can contain.
+our $_N_SYMBOLS_PER_QUERY = 30;
 
-our $_ERROR_DATE = '0000-00-00';
-
-our $_N_SYMBOLS_PER_REQUEST = 30;
+# The maximum number of pages to follow.
+our $_N_PAGES_PER_QUERY = 3;
 
 sub methods {
     return (yahoo_japan => \&yahoo_japan);
 }
 
 sub labels {
-    return (yahoo_japan => ['method', 'success', 'name', 'date', 'time', 'currency', 'price']);
+    return (yahoo_japan => ['method', 'success', 'name', 'date', 'time', 'currency', 'price', 'errormsg']);
 }
 
 sub yahoo_japan {
     my ($quoter, @symbols) = @_;
     return unless @symbols; # do nothing if no symbols.
 
-    my %info = ();
     my $ua = $quoter->user_agent;
+    my $url_base = 'http://info.finance.yahoo.co.jp/search/?ei=UTF-8&view=l1';
 
-    my $page = 1;
-    # limit the number of symbols a request contains.
-    while (my @syms = splice @symbols, 0, $_N_SYMBOLS_PER_REQUEST) {
-        my $paging = 1;
-        for (my $page = 1; $paging == 1; $page++) {
-            my $url = $YAHOO_JAPAN_URL . '/?ei=UTF-8&view=l1&p=' . $page . '&query=' . join '+', @syms;
-            my $reply = $ua->request(GET $url);
-            if ($reply->is_success) {
-                my $attrs = _get_page_attrs($reply->content, $page);
-                if ($attrs->{single} ne '') {
-                    %info = (%info, _web_scrape_single($reply->content, @syms));
+    my %info = ();
+    my @retry_later = ();
+
+    # initial trial loop: ignore page links.
+    while (my @syms = splice @symbols, 0, $_N_SYMBOLS_PER_QUERY) {
+        my $url = $url_base . '&query=' . join '+', @syms;
+        my $reply = $ua->request(GET $url);
+        if ($reply->is_success) {
+            my $tree = HTML::TreeBuilder->new_from_content($reply->content);
+            my %quotes = _scrape($tree);
+            my $has_next_page = _has_next_page($tree, 1);
+            $tree->delete;  # detach memory
+
+            for my $sym (@syms) {
+                next if ($info{$sym, 'success'});
+                if (exists $quotes{$sym}) {
+                    %info = (%info, _convert_quote($sym, $quotes{$sym}));
+                } elsif ($has_next_page) {
+                    push @retry_later, $sym;
                 } else {
-                    %info = (%info, _web_scrape($reply->content, @syms));
+                    $info{$sym, 'success'}  = 0;
+                    $info{$sym, 'method'}   = 'yahoo_japan';
+                    $info{$sym, 'errormsg'} = 'Requested quote not found.';
                 }
-                if ($attrs->{next} ne 'next') {
-                    $paging = 0;
-                }
+            }
+        }
+    }
+
+    # retry loop: follow page links.
+    while (my @syms = splice @retry_later, 0, $_N_SYMBOLS_PER_QUERY) {
+        my %quotes = ();
+        my $url = $url_base . '&query=' . join '+', @syms;
+        for (my $page = 1; $page <= $_N_PAGES_PER_QUERY; $page++) {
+            my $reply = $ua->request(GET $url . '&p=' . $page);
+            if ($reply->is_success) {
+                my $tree = HTML::TreeBuilder->new_from_content($reply->content);
+                %quotes = (%quotes, _scrape($tree));
+                my $has_next_page = _has_next_page($tree, $page);
+                $tree->delete;  # detach memory
+
+                last if (!$has_next_page);
+            }
+        }
+        for my $sym (@syms) {
+            next if ($info{$sym, 'success'});
+            if (exists $quotes{$sym}) {
+                %info = (%info, _convert_quote($sym, $quotes{$sym}));
             } else {
-                $paging = 0;
+                $info{$sym, 'success'}  = 0;
+                $info{$sym, 'method'}   = 'yahoo_japan';
+                $info{$sym, 'errormsg'} = 'Requested quote not found.';
             }
         }
     }
@@ -63,200 +96,115 @@ sub yahoo_japan {
     return \%info;
 }
 
-sub _get_page_attrs($;@) {
-    my ($content, $current_page) = @_;
-    my ($single, $next) = ('', '');
+# Tests if a list page has the next page of it.
+sub _has_next_page {
+    my $tree = shift;
+    my $current_page = shift;
 
-    my $tree = HTML::TreeBuilder->new;
-    $tree->utf8_mode(1);
-    $tree->parse($content);
-    $tree->eof();
-
-    my $single_element = $tree->look_down('id', 'divAddPortfolio');
-    if (defined  $single_element) {
-        $single = 'single';
+    my $elm_paging = $tree->look_down('class', 'ymuiPagingBottom clearFix');
+    if (defined $elm_paging) {
+        for my $page_link ($elm_paging->find('a')) {
+            my $num = $page_link->as_text;
+            return 1 if ($num =~ /^\d+$/ && $num == $current_page + 1);
+        }
     }
-    my $next_element = $tree->look_down('class', 'ymuiPagingBottom clearFix');
 
-    # when find next page link, go next page.
-    my @pagination = $next_element->find('a') if (defined $next_element);
-    foreach my $pageNum (@pagination) {
-        if ($pageNum->as_text =~ /^\d+$/) {
-            if ($pageNum->as_text == $current_page + 1) {
-                $next = 'next';
-                last;
-            } else {
-                $next = '';
+    return 0;
+}
+
+# Converts an internal quote data to Finance::Quote-style one.
+sub _convert_quote {
+    my ($sym, $quote) = @_;
+    my %info = ();
+    $info{$sym, 'success'}  = 1;
+    $info{$sym, 'currency'} = 'JPY';
+    $info{$sym, 'method'}   = 'yahoo_japan';
+    $info{$sym, 'name'}     = $quote->{'name'};
+    $info{$sym, 'date'}     = $quote->{'date'};
+    $info{$sym, 'time'}     = $quote->{'time'};
+    $info{$sym, 'price'}    = $quote->{'price'};
+    $info{$sym, 'errormsg'} = '';
+    return %info;
+}
+
+sub _scrape {
+    my $tree = shift;
+
+    # determine whether it is a single page or list page.
+    my $elm_single_marker = $tree->look_down('class', 'stocksDtl');
+    return (defined $elm_single_marker) ? _scrape_single_page($tree)
+                                        : _scrape_list_page($tree);
+}
+
+sub _scrape_single_page {
+    my $tree = shift;
+
+    $tree = $tree->look_down('class', 'stocksDtl');
+    my $elm_code     = $tree->look_down('class', 'stocksInfo')->find('dt');
+    my $elm_price    = $tree->look_down('class', 'stoksPrice');
+    my $elm_name     = $tree->look_down('class', 'symbol')->find('h1');
+    my $elm_datetime = $tree->look_down('class', 'yjSb real')->find('span');
+
+    my $sym = $elm_code->as_text;
+    my $stock_info = {
+        name  => $elm_name->as_text,
+        price => $elm_price->as_text,
+        date  => _parse_date($elm_datetime->as_text),
+        time  => _parse_time($elm_datetime->as_text)
+    };
+    $stock_info->{'price'} =~ tr/0-9//cd;
+    return ($sym => $stock_info);
+}
+
+sub _scrape_list_page {
+    my $tree = shift;
+    my %quotes = ();
+
+    my $elm_table = $tree->look_down('class', 'selectLine');    # XXX
+    if (defined $elm_table) {
+        for my $tr ($elm_table->find('tr')) {
+            if (my @row = $tr->find('td')) {
+                my $sym = $row[0]->as_text;
+                $quotes{$sym} = {
+                    name  => $row[2]->as_text,
+                    price => $row[4]->as_text,
+                    date  => _parse_date($row[3]->as_text),
+                    time  => _parse_time($row[3]->as_text)
+                };
+                $quotes{$sym}->{'price'} =~ tr/0-9//cd;
             }
         }
     }
 
-    my $attrs = {
-        single => $single,
-        next => $next
-    };
-    $tree->delete();
-    return $attrs;
-}
-
-# scarpe single
-sub _web_scrape_single($;@) {
-    my ($content, $symbol) = @_;
-    my %info = ();
-    my %stocks = ();
-
-    %stocks = _web_scrape_single_by_parse($content, $symbol);
-
-    $info{$symbol, 'symbol'} = $symbol;
-    $info{$symbol, 'success'} = 1;
-    $info{$symbol, 'currency'} = 'JPY';
-    $info{$symbol, 'method'} = 'yahoo_japan';
-    $info{$symbol, 'name'} = $stocks{$symbol}->{name};
-    $info{$symbol, 'date'} = $stocks{$symbol}->{date};
-    $info{$symbol, 'time'} = $stocks{$symbol}->{time};
-    my $price = $stocks{$symbol}->{price};
-    $price =~ s/,//g;
-    $info{$symbol, 'price'} = $price;
-    $info{$symbol, 'errormsg'} = '';
-
-    return %info;
-}
-
-sub _web_scrape_single_by_parse($;@) {
-    my ($content, $symbol) = @_;
-    my %stocks = ();
-    my $tree = HTML::TreeBuilder->new;
-    $tree->utf8_mode(1);
-    $tree->parse($content);
-    $tree->eof();
-
-    my $stock_element = $tree->look_down('class', 'stocksInfo')->find('dt');
-    my $price_element = $tree->look_down('class', 'stoksPrice');
-    my $name_element = $tree->look_down('class', 'symbol')->find('h1');
-    my $date_element = $tree->look_down('class', 'yjSb real')->find('span');
-    my $stock_info = {
-        code => $stock_element->as_text,
-        name => $name_element->as_text,
-        price => $price_element->as_text,
-        date => _get_date($date_element->as_text),
-        time => _get_time($date_element->as_text)
-    };
-    # detach memory
-    $tree->delete();
-
-    $stocks{$symbol} = $stock_info;
-    return %stocks;
-}
-
-# web scrape
-sub _web_scrape($;@) {
-    my ($content, @symbols) = @_;
-    my %info = ();
-    my %stocks = ();
-
-    %stocks = _web_scrape_by_parse($content, @symbols);
-
-    foreach my $symbol (@symbols) {
-        if (defined $stocks{$symbol}) {
-            $info{$symbol, 'symbol'} = $symbol;
-            $info{$symbol, 'success'} = 1;
-            $info{$symbol, 'currency'} = 'JPY';
-            $info{$symbol, 'method'} = 'yahoo_japan';
-            $info{$symbol, 'name'} = $stocks{$symbol}->{name};
-            $info{$symbol, 'date'} = $stocks{$symbol}->{date};
-            $info{$symbol, 'time'} = $stocks{$symbol}->{time};
-            my $price = $stocks{$symbol}->{price};
-            $price =~ s/,//g;
-            $info{$symbol, 'price'} = $price;
-            $info{$symbol, 'errormsg'} = '';
-        }
-    }
-
-    return %info;
-}
-
-sub _web_scrape_by_parse($;@) {
-    my ($content, @symbols) = @_;
-    my $tree = HTML::TreeBuilder->new;
-    my %stocks = ();
-    $tree->utf8_mode(1);
-    $tree->parse($content);
-    $tree->eof();
-
-    my $tableLine = $tree->look_down('class', 'selectLine');
-    my @elements = $tableLine->find('tr') if (defined $tableLine);
-    foreach my $element (@elements) {
-        my @row = $element->find('td');
-        if (defined($row[0])) {
-            my $stock_info = {
-                code => $row[0]->as_text,
-                name => $row[2]->as_text,
-                price => $row[4]->as_text,
-                date => _get_date($row[3]->as_text),
-                time => _get_time($row[3]->as_text)
-            };
-            $stocks{$row[0]->as_text} = $stock_info;
-        }
-    }
-    # detach memory
-    $tree->delete();
-    return %stocks;
-}
-
-sub _get_date($;@) {
-    my ($value) = @_;
-    my ($date);
-
-    if (defined $value) {
-        if (index($value, '/') != -1) {
-            $date = _parse_date($value);
-        } else {
-            my @now = localtime;
-            $date = _parse_date($now[4]+1 . '/' . $now[3]);
-        }
-    } else {
-        my @now = localtime;
-        $date = _parse_date($now[4]+1 . '/' . $now[3]);
-    }
-    return $date;
-}
-
-sub _get_time($;@) {
-    my ($value) = @_;
-    my $time = '15:00:00';
-
-    if (defined $value) {
-        if (index($value, '/') != -1) {
-            $time = '15:00:00';
-        } else {
-            $time = _parse_time($value);
-        }
-    }
-    return $time;
+    return %quotes;
 }
 
 # Determines the date of a quote.
 sub _parse_date($;) {
-    my ($date, @now) = (shift, localtime);
+    my $date = shift;
+    my @now = localtime;
+    my ($yyyy, $mm, $dd) = ($now[5] + 1900, $now[4] + 1, $now[3]);  # XXX
+
     if ($date =~ /(\d{1,2})\/(\d{1,2})/) {
         # MM/DD
-        my ($yyyy, $mm, $dd) = ($now[5] + 1900, $1, $2);
+        ($mm, $dd) = ($1, $2);
         $yyyy-- if ($now[4] + 1 < $mm); # MM may point last December in January.
-        return sprintf '%04d-%02d-%02d', $yyyy, $mm, $dd;
-    } else {
-        return $_ERROR_DATE;
     }
+
+    return sprintf '%04d-%02d-%02d', $yyyy, $mm, $dd;
 }
 
 # Determines the time of a quote.
 sub _parse_time($;) {
     my $time = shift;
+    my ($hh, $mm) = (15, 0);  # XXX return 15:00:00 on error.
+
     if ($time =~ /(\d{1,2}):(\d{1,2})/) {
         # HH:MM
-        return sprintf '%02d:%02d:00', $1, $2;
-    } else {
-        return '15:00:00';  # XXX
+        ($hh, $mm) = ($1, $2);
     }
+
+    return sprintf '%02d:%02d:00', $hh, $mm;
 }
 
 1;
